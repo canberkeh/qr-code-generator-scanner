e4b1e3ca62f1845d576804d5f6e2919161cc8be2
berke-horozal-crea <90218881+berke-horozal-crea@users.noreply.github.com>
2021-09-15 13:22:05 +0300

    update generator, set logging placements

--- generator.py
+++ generator.py
@@ -5,23 +5,31 @@ Generated Qr will be opened and you will see a feedback on terminal.
 '''
 import qrcode
 from PIL import Image
+from log import logger
+import os
 
-
+logging = logger()
 class QrGenerator():
-    def generate_qr_code():
 
+    def generate_qr_code():
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=4,
         )
-        qr.add_data("SPECIFY YOUR DATA/LINK HERE")
-        qr.make(fit=True)
-        img = qr.make_image(fill_color="red", back_color="black")
-        img.save("yourqrcode.png")
+        try:
+            qr.add_data("SPECIFY YOUR DATA/LINK HERE")
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="red", back_color="black")
+            img.save("yourqrcode.png")
+        except:
+            print("Cannot prepare qr code! Check you data.")
+            logging.error('given data is not valid.')
         opening_image = Image.open("yourqrcode.png")
         opening_image.show()
+        print(opening_image.filename)
+        logging.info(f'Qr Code has been generated. Name : {opening_image.filename}')
         print("Your qr code has been generated. Shutting down.")
         controller = False
         return controller
--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def terminator():
             choice = input("1 - QrGenerator\n2 - QrScanner(Remember ctrl+c to exit!)\nquit()\n")
             if choice == "1":
                 from generator import QrGenerator
-                logging.info(f'{QrGenerator.generate_qr_code} method has been terminated.')
+                logging.info(f'{QrGenerator.generate_qr_code.__name__} method has been terminated.')
                 controller = QrGenerator.generate_qr_code()
             elif choice == "2":
                 from scanner import QrScanner
@@ -25,12 +25,11 @@ def terminator():
                 controller=False
                 print("Take care!")
         except:
-            if choice != "quit()" and isinstance(choice, str):
-                print("Only numbers!\n")
-                logging.warning(f'Wrong input type {choice} type ==> {type(choice)}')
-            elif choice is None:
+            if choice is None:
                 print("Input needed!\n")
                 logging.warning(f'Nonetype input, type ==> {type(choice)}')
+        finally:
+            controller = False
 
 
 if __name__ == '__main__':
